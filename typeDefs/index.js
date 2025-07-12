0c49e74d852c5d04f7bb02c892d916ccc74f0b45
antor-arif <rahmanarifur4838@gmail.com>
2025-07-12 21:38:11 +0600

    completed type definition of books

--- typeDefs/index.js
+++ typeDefs/index.js
@@ -1,8 +1,18 @@
 const typeDefs = `#graphql
-  
+
   type Book {
     title: String
     author: String
+    description: String
+    publishedDate: String
+    genre: String
+    pages: Int
+    coverImage: String
+    price: Float
+    stock: Int
+    ratings: Float
+    discountPrice: Float
+    couponCode: String
   }
 
   type Query {
